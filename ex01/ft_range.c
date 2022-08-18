5957c9551a8e143941397b733ac7d054f96b2bcd
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-08-18 12:50:18 +0200

    finished ex01

---
+++ ex01/ft_range.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*range;
+	int	size;
+	int	i;
+
+	if (min >= max)
+		return ((int *)0);
+	size = max - min;
+	if (size < 0)
+		size = size * -1;
+	range = malloc(sizeof(int) * size);
+	i = min;
+	while (i < size)
+	{
+		range[i] = min + i;
+		i++;
+	}
+	return (range);
+}
+
